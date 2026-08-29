5e453abae3bef5fbb3d3df4b7be97981d9219344
Harish-045 <harishkiccha45@gmail.com>
2026-08-29 23:18:37 +0530

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+   
+    public int minCostClimbingStairs(int[] cost) {
+        int n=cost.length;
+        int next1=0;
+        int next2=0;
+        for(int i=n-1;i>=0;i--){
+            int cur=cost[i]+Math.min(next1,next2);
+            next2=next1;
+            next1=cur;
+        }
+        return Math.min(next1,next2);
+    }
+}
