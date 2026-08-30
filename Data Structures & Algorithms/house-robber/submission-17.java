59994f7e9659eca6e41359a60c3f5cbddb73ffac
Harish-045 <harishkiccha45@gmail.com>
2026-08-30 14:31:30 +0530

    Add: house-robber - submission-17

---
+++ Data Structures & Algorithms/house-robber/submission-17.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int rob(int[] nums) {
+       int n=nums.length;
+       int prev=nums[0];
+       int prev2=0;
+        
+
+       for(int i=1;i<n;i++){
+           int take=nums[i];
+           if(i>1){
+            take+=prev2;
+           }
+           int not=0+prev;
+           int cur=Math.max(take,not);
+           prev2=prev;
+           prev=cur;
+       }
+       return prev;
+    }
+}
